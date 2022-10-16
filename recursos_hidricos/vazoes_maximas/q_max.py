9accc96e95ca9f3fa3f76ac7d6156ecf5774982f
wasf84 <wfilho@ice.ufjf.br>
2022-10-16 20:40:46 -0300

    finalizado o trabalho de Recurso Hidricos - incial; esta plotando o grafico com os graficos todos juntos.

--- recursos_hidricos/vazoes_maximas/q_max.py
+++ recursos_hidricos/vazoes_maximas/q_max.py
@@ -31,7 +31,7 @@ def imprimirTabela(dataFrame, posicao):
 # a primeira linha corresponde aos meses do ano
 # a primeira coluna corresponde aos anos observados
 #dados_csv = pd.read_csv('vazoes_maximas_teste.csv', delimiter=';', header=0, index_col=0, on_bad_lines='warn')
-dados_csv = pd.read_csv('vazoes_maximas_trabalho.csv', delimiter=';', header=0, index_col=0, on_bad_lines='warn')
+dados_csv = pd.read_csv('vazoes_maximas_capibaribe.csv', delimiter=';', header=0, index_col=0, on_bad_lines='warn')
 #dados_csv.T.plot(legend=False)
 
 
@@ -101,18 +101,11 @@ tempo_retorno = [1/prob_empirica[x] for x in tqdm.tqdm(range(len(prob_empirica))
 # In[10]:
 
 
-df_plot = pd.DataFrame({'QMax Empírica (m3/s)': q_max_sort, 'Tempo de Retorno': tempo_retorno})
-df_plot.plot.scatter(x='Tempo de Retorno', y='QMax Empírica (m3/s)', grid=True)
+#df_plot = pd.DataFrame({'QMax Empírica (m3/s)': q_max_sort, 'Tempo de Retorno': tempo_retorno})
+#df_plot.plot.scatter(x='Tempo de Retorno', y='QMax Empírica (m3/s)', grid=True)
 
 
 # In[11]:
-
-
-#len(tempo_retorno), tempo_retorno
-#len(q_max_sort), q_max_sort
-
-
-# In[12]:
 
 
 # Gumbel
@@ -134,7 +127,7 @@ u = m - (0.5772 * a)
 
 # Gumbel TR
 q_tr = []
-gumbel_tr = [(i+1.)*10 for i in tqdm.tqdm(range(len(q_max)))]
+gumbel_tr = [(i+1.) for i in tqdm.tqdm(range(len(q_max)))]
 gumbel_tr[0] = 1.01 # essa manha tem que ser feita porque senão retorna 0 para o 'ln' e isso é matematicamente errado.
    
 q_tr = [u - (np.log(-np.log(1 - 1/gumbel_tr[i])) * a) for i in tqdm.tqdm(range(len(gumbel_tr)))]
@@ -142,46 +135,12 @@ q_tr = [u - (np.log(-np.log(1 - 1/gumbel_tr[i])) * a) for i in tqdm.tqdm(range(l
 #m, s, a, u, q_tr
 
 
-# In[13]:
-
-
-#len(q_max_sort), len(gumbel_tr), len(q_tr)
-
-
-# In[14]:
-
-
-# Esse passo é importante para desenhar o mesmo gráfico com cores diferentes para os pontos
-n_vazoes = len(q_max_sort) + len(q_tr)
-
-#vetor_final_plot[0] = [q_max_sort[0], tempo_retorno[0], 1] <<< ISSO FUNCIONA
-vetor_final_plot = []
-for i in tqdm.tqdm(range(n_vazoes)):
-    if (i < int(n_vazoes/2)):
-        vetor_final_plot.insert(i, [q_max_sort[i], tempo_retorno[i], 1]) # Empirico
-    else:
-        vetor_final_plot.insert(i, [q_tr[i-int(n_vazoes/2)], gumbel_tr[i-int(n_vazoes/2)], 2]) # Gumbel
-
-gumbel_plot = pd.DataFrame(vetor_final_plot, columns=['Vazão (m3/2)', 'TR', 'Tipo'])
-#gumbel_plot = pd.DataFrame({'QMax Gumbel (m3/s)': q_tr, 'QMax Empírica (m3/s)' : q_max_sort, 'TR Gumbel': gumbel_tr, 'TR Empírico': tempo_retorno})
-#imprimirTabela(gumbel_plot, 'center')
-#gumbel_plot.plot(x='TR Gumbel', y=['QMax Gumbelc (m3/s)', 'QMax Empírica (m3/s)'])
-gumbel_plot.plot.scatter(x='TR', y='Vazão (m3/2)', c='Tipo', colormap='Set1', title='Gumbel', grid=True)
-
-
-# In[15]:
-
-
-(5, 2)
-
-
-# In[16]:
+# In[12]:
 
 
 # Log-Pearson T3
 
-# Gera o vetor com LN de cada uma das vazões máximas
-#log_qmax = [np.log(q_max[i]) for i in tqdm.tqdm(range(len(q_max)))]
+# Gera o vetor com LOG de cada uma das vazões máximas
 log_qmax = np.log10(q_max)
 
 # Para calcula a MODA
@@ -192,7 +151,7 @@ log_qmax_discreto = [int(log_qmax[i]) for i in tqdm.tqdm(range(len(log_qmax)))]
 me, mo, sd = st.mean(log_qmax), st.mode(log_qmax_discreto), st.stdev(log_qmax)
 g = (me - mo) / sd
 
-vetor_tr_pearson = [(i+1.)*10 for i in tqdm.tqdm(range(len(q_max)))]
+vetor_tr_pearson = [(i+1.) for i in tqdm.tqdm(range(len(q_max)))]
 vetor_tr_pearson[0] = 1.01
 
 vetor_t = [np.sqrt(2 * np.log(vetor_tr_pearson[i])) for i in tqdm.tqdm(range(len(vetor_tr_pearson)))]
@@ -203,9 +162,46 @@ vetor_k = [2/g*(np.power(((vetor_k1[i]-(g/6))*(g/6)+1),3)-1) for i in tqdm.tqdm(
 # 10^((media dos log10 das vazoes) + K*S_log10_Q)
 pearson_tr = [np.power(10, (me+vetor_k[i]*sd)) for i in tqdm.tqdm(range(len(vetor_k)))]
 
-print("\n me: %s \n mo: %s \n sd: %s \n g: %s \n log_qmax: %s \n log_qmax_discreto: %s \
-      \n vetor_t: %s \n vetor_k1: %s \n vetor_k: %s \n pearson_tr: %s" % \
-      (me, mo, sd, g, log_qmax, log_qmax_discreto, vetor_t, vetor_k1, vetor_k, pearson_tr))
+#print("\n me: %s \n mo: %s \n sd: %s \n g: %s \n log_qmax: %s \n log_qmax_discreto: %s \
+#      \n vetor_t: %s \n vetor_k1: %s \n vetor_k: %s \n pearson_tr: %s" % \
+#      (me, mo, sd, g, log_qmax, log_qmax_discreto, vetor_t, vetor_k1, vetor_k, pearson_tr))
 
-pearson_plot = pd.DataFrame({'QMax (m3/s)' : pearson_tr, 'TR' : vetor_tr_pearson})
-pearson_plot.plot.scatter(x='TR', y='QMax (m3/s)', title='Log-Pearson Tipo III', c='DarkGreen')
+
+# In[13]:
+
+
+# Esse passo é importante para desenhar o mesmo gráfico com cores diferentes para os pontos
+n_vazoes = len(q_max_sort) + len(q_tr) + len(vetor_tr_pearson)
+
+#vetor_final_plot[0] = [q_max_sort[0], tempo_retorno[0], 1] <<< ISSO FUNCIONA
+vetor_final_plot = []
+for i in tqdm.tqdm(range(n_vazoes)):
+    if (i < int(n_vazoes/3)): # de 0 a 30
+        vetor_final_plot.insert(i, [q_max_sort[i], tempo_retorno[i], 1]) # Empirico
+    elif (i >= int(n_vazoes/3) and i < 2*int(n_vazoes/3)): # de 31 a 61
+        vetor_final_plot.insert(i, [q_tr[i-2*int(n_vazoes/3)], gumbel_tr[i-2*int(n_vazoes/3)], 2]) # Gumbel
+    else:  # de 62 a 92
+        vetor_final_plot.insert(i, [pearson_tr[i-3*int(n_vazoes/3)], vetor_tr_pearson[i-3*int(n_vazoes/3)], 3]) # Pearson
+        
+final_plot = pd.DataFrame(vetor_final_plot, columns=['Vazão (m3/s)', 'TR', 'Tipo'])
+#imprimirTabela(final_plot, 'center')
+final_plot.plot.scatter(x='TR', y='Vazão (m3/s)', c='Tipo', colormap='Set1', title='Dados empíricos + Gumbel + Log-Pearson', grid=True)
+
+
+# In[14]:
+
+
+# Esse passo é importante para desenhar o mesmo gráfico com cores diferentes para os pontos
+#n_vazoes = len(q_max_sort) + len(q_tr)
+
+#vetor_final_plot[0] = [q_max_sort[0], tempo_retorno[0], 1] <<< ISSO FUNCIONA
+#vetor_final_plot = []
+#for i in tqdm.tqdm(range(n_vazoes)):
+#    if (i < int(n_vazoes/2)):
+#        vetor_final_plot.insert(i, [q_max_sort[i], tempo_retorno[i], 1]) # Empirico
+#    else:
+#        vetor_final_plot.insert(i, [q_tr[i-int(n_vazoes/2)], gumbel_tr[i-int(n_vazoes/2)], 2]) # Gumbel
+
+#gumbel_plot = pd.DataFrame(vetor_final_plot, columns=['Vazão (m3/s)', 'TR', 'Tipo'])
+#gumbel_plot.plot.scatter(x='TR', y='Vazão (m3/s)', c='Tipo', colormap='Set1', title='Gumbel', grid=True)
+
